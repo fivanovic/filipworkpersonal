700a8a9b493dcbfabfc408df7b123f969a9d12a2
fivanovic <fivanovic1999@gmail.com>
2021-08-30 15:32:12 +0300

    first stage of code

--- relaycode.py
+++ relaycode.py
@@ -1,1 +1,17 @@
+import time
+import pigpio
+pi = pigpio.pi()
+RELAY1 = 26
+RELAY2 = 20
+RELAY3 = 21
+pi.set_mode(RELAY1,pigpio.OUTPUT)
+pi.set_mode(RELAY2,pigpio.OUTPUT)
+pi.set_mode(RELAY3,pigpio.OUTPUT)
 
+BUTTON1 = 2
+BUTTON2 = 3
+BUTTON3 = 4
+
+while True:
+    selection = input("Select Testing Device")
+    print(selection)
