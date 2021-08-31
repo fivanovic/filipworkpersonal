f785879f2f56f7faae1d41c7ae0d58f5fb2ed36c
fivanovic <fivanovic1999@gmail.com>
2021-08-31 15:01:48 +0300

    Commenting out code based on toggle button logic

--- relaycode.py
+++ relaycode.py
@@ -40,25 +40,25 @@ def RelayController(rel, reloff1, reloff2):
     pi.write(rel.numb,0)
     pi.gpio_trigger(rel.button, 10, 1) #needs to be looked at in terms of polarity of buttons and potential necessity of pullup resistor
     rel.status = 1
-    if reloff1.status == 1:
-        pi.gpio_trigger(reloff1.button, 10, 1)
-        reloff1.status = 0
-    if reloff2.status == 1:
-        pi.gpio_trigger(reloff2.button, 10 ,1)
-        reloff2.status = 0
+    #if reloff1.status == 1:
+        #pi.gpio_trigger(reloff1.button, 10, 1)
+        #reloff1.status = 0
+    #if reloff2.status == 1:
+        #pi.gpio_trigger(reloff2.button, 10 ,1)
+        #reloff2.status = 0
 
 
 def RelayClear(rel1, rel2, rel3):
     print("Clearing All relays, turning off all devices")
-    if rel1.status == 1:
-        pi.gpio_trigger(rel1.button, 10, 1)
-        rel1.status = 0
-    if rel2.status == 1:
-        pi.gpio_trigger(rel2.button, 10, 1)
-        rel2.status = 0
-    if rel3.status == 1:
-        pi.gpio_trigger(rel3.button, 10, 1)
-        rel3.status = 0
+    #if rel1.status == 1:
+        #pi.gpio_trigger(rel1.button, 10, 1)
+        #rel1.status = 0
+    #if rel2.status == 1:
+        #pi.gpio_trigger(rel2.button, 10, 1)
+        #rel2.status = 0
+    #if rel3.status == 1:
+        #pi.gpio_trigger(rel3.button, 10, 1)
+        #rel3.status = 0
     pi.write(rel1.numb, 1)
     pi.write(rel2.numb, 1)
     pi.write(rel3.numb, 1)
