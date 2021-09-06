6c788f9bcb19037c44718dfad72e49e22fd27541
fivanovic <fivanovic1999@gmail.com>
2021-09-06 12:33:37 +0200

    id and state

--- RelayApi.py
+++ RelayApi.py
@@ -101,7 +101,7 @@ class Pin(Resource):
         #"""update a pin given its identifier"""
         #return pin_util.update(id, api.payload)
 
-    @ns.expect(pin_model['state'])
+    @ns.expect(pin_model['id', 'state'])
     @ns.marshal_with(pin_model['state'])
     def patch(self, id):
         """partially update a pin given its identifier"""
